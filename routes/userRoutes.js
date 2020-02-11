386f4406cbd963566aed51f15c2c9ae14c749a4c
John Stilwell <johnrstilwell@codeimmersives.com>
2020-02-11 13:36:29 -0500

    cleanup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../models/Users')
-
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllUsers)
